6428d498a8853f1482e06647f2df231b00da2176
Gregory DESCAMPS <descamps.gregory@gmail.com>
2025-11-27 16:29:20 +0100

    Refactor setup script to install from requirements.txt and comment out dvc pull

--- 1_setup.sh
+++ 1_setup.sh
@@ -3,9 +3,8 @@ rm -rf venv
 python3.11 -m venv venv
 source venv/bin/activate
 pip install -U packaging==23.2 setuptools==75.8.0 wheel ninja
-pip install torch
-pip install -r requirements_freeze.txt
+pip install -r requirements.txt
 
 pip install -e .
-dvc pull
+#dvc pull
 
